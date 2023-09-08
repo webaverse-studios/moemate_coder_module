7bdea92e06f46dabeacfab5feda1bb09c061fac6
gonnavis <gonnavis@163.com>
2023-09-08 17:47:42 +0800

    OnlyReplyHtml

--- scripts/coder.js
+++ scripts/coder.js
@@ -2,6 +2,7 @@
 const messages = [];
 window.messages = messages;
 const errorOccuredIframeIds = [];
+const lastConsoleLog = '';
 
 function extractCode(inputString) {
   const regex = /```([a-zA-Z]+)\n([\s\S]*?)```/g;
@@ -40,127 +41,122 @@ async function callModel(newMessages = []) {
   console.log('--- responseContent:', responseContent)
   messages.push({role: 'assistant', content: responseContent});
 
-  const result = eval(responseContent);
+  // const responseObj = extractCode(responseContent);
+  const responseObj = JSON.parse(responseContent);
+  console.log('--- responseObj:', responseObj);
 
-  setTimeout(async () => { // ensure the triggering of hack_delay. // todo: Prmoise.all // todo: don't await above
-    window.hooks.emit("hack_delay", `Tell user the result is ${result}`);
-    // window.companion.SendMessage({ type: "ASK_DOCTOR", user: name, value: `Got diagnosis info`, timestamp: Date.now(), alt: 'alt' });
-  }, 100);
-  return;
-
-  const codeObj = extractCode(responseContent);
-  console.log('--- codeObj:', codeObj);
-
-  if (codeObj.language.toLowerCase() === 'javascript') {
-    const result = eval(codeObj.codeBlock);
-
-    setTimeout(async () => { // ensure the triggering of hack_delay. // todo: Prmoise.all // todo: don't await above
-      if (codeObj.language === 'javascript') {
-        eval(codeObj.codeBlock);
-        window.hooks.emit("hack_delay", `Tell user the result is ${result}`);
-        // window.companion.SendMessage({ type: "ASK_DOCTOR", user: name, value: `Got diagnosis info`, timestamp: Date.now(), alt: 'alt' });
+  // handle errors
+  window.addEventListener('message', function(event) { // todo: removEventListener
+    if (event.data.type === 'IFRAME_ERROR') {
+      if (errorOccuredIframeIds.includes(event.data.iframeId)) {
+        return; // only handle first error. // todo: handle all errors at once ?
       }
-    }, 100);
-  } else if (codeObj.language.toLowerCase() === 'html') {
+      errorOccuredIframeIds.push(event.data.iframeId);
+      console.log('--- error from iframe:', event.data.data);
+      debugger
+      callModel([
+        {role:'user',content:'I got this error: ' + JSON.stringify(event.data.data)}
+      ])
+    } else if (event.data.type === 'IFRAME_INFO') {
+      lastConsoleLog = event.data.data;
+      console.log('--- lastConsoleLog:', lastConsoleLog);
+    }
+  });
 
-    // handle errors
-    window.addEventListener('message', function(event) { // todo: removEventListener
-      if (event.data.type === 'IFRAME_ERROR') {
-        if (errorOccuredIframeIds.includes(event.data.iframeId)) {
-          return; // only handle first error. // todo: handle all errors at once ?
-        }
-        errorOccuredIframeIds.push(event.data.iframeId);
-        console.log('--- error from iframe:', event.data.data);
-        debugger
-        callModel([
-          {role:'user',content:'I got this error: ' + JSON.stringify(event.data.data)}
-        ])
+  // 1. Create an iframe element dynamically
+  var iframe = document.createElement('iframe');
+  iframe.id = Math.random();
+
+  // 2. Set iframe attributes (optional)
+  iframe.src = 'about:blank'; // You can set the source URL
+  iframe.width = '300'; // Set the width
+  iframe.height = '200'; // Set the height
+
+  // 3. Append the iframe to the document
+  document.body.appendChild(iframe);
+
+  const scriptBefore = `
+    <script>
+
+      // handle console.log
+      const originalConsoleLog = console.log;
+      console.log = function (...args) {
+        console.log('--- log from console.log', args);
+        window.parent.postMessage({type: 'IFRAME_INFO', iframeId: ${iframe.id}, data: args}, '*');
+        const logMessage = args.join(' ');
+        originalConsoleLog.apply(console, args);
+      };
+
+      // handle code logic error
+      window.addEventListener('error', ({message, lineno, colno}) => {
+        // debugger
+        console.log('--- iframe error:', {message, lineno, colno});
+        window.parent.postMessage({type: 'IFRAME_ERROR', iframeId: ${iframe.id}, data: {message, lineno, colno}}, '*');
+      });
+      
+      // handle createElement error, such as wrong element.src
+      const originalCreateElement = document.createElement;
+      document.createElement = function(tagName) {
+        const element = originalCreateElement.call(document, tagName);
+        element.addEventListener('error', function(event) {
+          console.log('--- error from document createElement', event);
+          /* event
+            isTrusted: true
+            bubbles: false
+            cancelBubble: false
+            cancelable: false
+            composed: false
+            currentTarget: null
+            defaultPrevented: false
+            eventPhase: 0
+            returnValue: true
+            srcElement: img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive
+            target: img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive
+            timeStamp: 57.200000047683716
+            type: "error"
+          */
+          window.parent.postMessage({type: 'IFRAME_ERROR', iframeId: ${iframe.id}, data: '404 Not Found: ' + event.srcElement.src}, '*');
+        })
+        return element;
       }
-    });
+      
+      // handle console.error
+      const originalConsoleError = console.error;
+      console.error = function (...args) {
+        console.log('--- error from console.error', args);
+        window.parent.postMessage({type: 'IFRAME_ERROR', iframeId: ${iframe.id}, data: args}, '*');
+        const errorMessage = args.join(' ');
+        originalConsoleError.apply(console, args);
+      };
 
-    // 1. Create an iframe element dynamically
-    var iframe = document.createElement('iframe');
-    iframe.id = Math.random();
+    </script>
+  `;
+  const scriptAfter = `
+    <script>
+        // Get all script elements in the document
+        const scriptElements = document.querySelectorAll('script');
 
-    // 2. Set iframe attributes (optional)
-    iframe.src = 'about:blank'; // You can set the source URL
-    iframe.width = '300'; // Set the width
-    iframe.height = '200'; // Set the height
-
-    // 3. Append the iframe to the document
-    document.body.appendChild(iframe);
-
-    const handleErrorBefore = `
-      <script>
-
-        // handle code logic error
-        window.addEventListener('error', ({message, lineno, colno}) => {
-          // debugger
-          console.log('--- iframe error:', {message, lineno, colno});
-          window.parent.postMessage({type: 'IFRAME_ERROR', iframeId: ${iframe.id}, data: {message, lineno, colno}}, '*');
+        // Add an error event listener to each script element
+        scriptElements.forEach(script => {
+            script.addEventListener('error', (event) => {
+                // Handle script loading errors here
+                console.error('--- Error loading script:' + script.src, event);
+            });
         });
-        
-        // handle createElement error, such as wrong element.src
-        const originalCreateElement = document.createElement;
-        document.createElement = function(tagName) {
-          const element = originalCreateElement.call(document, tagName);
-          element.addEventListener('error', function(event) {
-            console.log('--- error from document createElement', event);
-            /* event
-              isTrusted: true
-              bubbles: false
-              cancelBubble: false
-              cancelable: false
-              composed: false
-              currentTarget: null
-              defaultPrevented: false
-              eventPhase: 0
-              returnValue: true
-              srcElement: img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive
-              target: img.leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive
-              timeStamp: 57.200000047683716
-              type: "error"
-            */
-            window.parent.postMessage({type: 'IFRAME_ERROR', iframeId: ${iframe.id}, data: '404 Not Found: ' + event.srcElement.src}, '*');
-          })
-          return element;
-        }
-        
-        // handle console.error
-        const originalConsoleError = console.error;
-        console.error = function (...args) {
-          console.log('--- error from console.error', args);
-          window.parent.postMessage({type: 'IFRAME_ERROR', iframeId: ${iframe.id}, data: args}, '*');
-          const errorMessage = args.join(' ');
-          originalConsoleError.apply(console, args);
-        };
+    </script>
+  `;
 
-      </script>
-    `;
-    const handleErrorAfter = `
-      <script>
-          // Get all script elements in the document
-          const scriptElements = document.querySelectorAll('script');
-
-          // Add an error event listener to each script element
-          scriptElements.forEach(script => {
-              script.addEventListener('error', (event) => {
-                  // Handle script loading errors here
-                  console.error('--- Error loading script:' + script.src, event);
-              });
-          });
-      </script>
-    `;
-
-    // 4. Access the iframe's content document and write HTML content into it
-    var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-    iframeDocument.open();
-    iframeDocument.write(handleErrorBefore + codeObj.codeBlock + handleErrorAfter);
-    iframeDocument.close();
-  } else {
-    throw new Error('Invalid code snippet language');
-  }
-  return codeObj;
+  // 4. Access the iframe's content document and write HTML content into it
+  var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+  iframeDocument.open();
+  iframeDocument.write(scriptBefore + responseObj.html + scriptAfter);
+  iframeDocument.close();
+  
+  setTimeout(async () => { // ensure the triggering of hack_delay. // todo: Prmoise.all // todo: don't await above
+    window.hooks.emit("hack_delay", `Tell user the result is ${lastConsoleLog}`);
+  }, 100);
+  
+  return responseObj;
 }
 window.callModel = callModel
 /* console_test
@@ -238,13 +234,21 @@ async function _handleCoderSkill() {
 
 You are role-playing as a professional javascript coder/programmer. You need to generate code to solve the user's question.
 
-You can only reply Javascript code block. I'll \`eval()\` your code and get the result, so don't \`console.log()\` the result.
+You can only reply full HTML code. (Must reply full HTML, which includes all the needed javascript code, css style, etc in it, can't separate javascript code and css style code to other code blocks.)
+If need to give a result to the user, \`console.log()\` it in the end of the js code.
 
 MUST NOT use scripts which require "token" or "key", the user WON'T obtain and provide it !!!
 DON'T use "Google Maps JavaScript API" or other apis which require "token" or "key" !!!
 DON'T ask the user to do anything, solve all the errors by yourself !!!
 Always provide FULL code, don't use partial code even when fixing errors, or you will be punished !!!
 You need to match the user's requirement as much as possible, prevent provide overly simplified result.
+
+Reply in JSON format:
+Example:
+{
+  "html": "...",
+  "explain": "..."
+}
 `},
 // You can ONLY use free resources, MUST NOT use src/url or api which includes "token", "ACCESS_TOKEN", "API_KEY", "YOUR_API_KEY" etc.
     {role: 'user', content: question},
@@ -277,12 +281,8 @@ You need to match the user's requirement as much as possible, prevent provide ov
     Finally, we call the `fibonacci` function with an input of `10` to get the 10th Fibonacci number and store it in the `tenthFibonacci` variable. We then use `console.log` to output the result to the console.
   */
 
-  // const responseObj = JSON.parse(response.choices[0].message.content)
-  // console.log('--- responseObj:', responseObj)
+  const responseObj = await callModel(newMessages);
 
-  const codeObj = await callModel(newMessages);
-
-  // return responseObj
 }
 window._handleCoderSkill = _handleCoderSkill; // test
 
